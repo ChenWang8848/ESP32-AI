03e73484b6d58ff9042f50f630ff7bc9393cb2d0
ChenChen <710466964@qq.com>
2025-07-23 14:53:05 +0800

    20250723实现音频编码功能

--- Server-side/ai_core/audo_format/opus.py
+++ Server-side/ai_core/audo_format/opus.py
@@ -1,4 +1,6 @@
 import os.path
+import wave
+
 import numpy as np
 from pydub import AudioSegment
 import opuslib_next
@@ -13,7 +15,7 @@ class Opus_Encoder:
         self.opus_channel_count = 1
         self.opus_sample_width = 2
         self.opus_frame_time = 60 # 会影响传送个esp32的速率
-
+        self.opus_frame_size = int(self.opus_sample_rate /  1000 * self.opus_frame_time )
     def audio_to_opus(self, audio_path):
         #解析文件路径
         file_type = os.path.splitext(audio_path)[1]
@@ -36,7 +38,7 @@ class Opus_Encoder:
         #初始化编码器
         encode = opuslib_next.Encoder(self.opus_sample_rate,self.opus_channel_count,opuslib_next.APPLICATION_AUDIO)
         #获取每帧的采样数,计算1ms的采样率，诚每帧的采样时间即可。
-        frame_num = int(self.opus_sample_rate /  1000 * self.opus_frame_time )
+        frame_num = self.opus_frame_size
         #计算每帧的采样字节数
         frame_byte_num = frame_num * self.opus_sample_width * self.opus_channel_count
         opus_dates = []
@@ -56,7 +58,49 @@ class Opus_Encoder:
             opus_dates.append(opus_data)
         return opus_dates,duration
 
+    # 保存二进制流的pous数据
+    def save_opus_raw(self, opus_data, save_path):
+        with open(save_path, 'wb') as f: # python中低层的迭代器，能够直接读取一帧一帧的数据。
+            for frame in opus_data:
+                f.write(len(frame).to_bytes(4,byteorder='big'))
+                f.write(frame)
+
+    def load_opus_raw(self, opus_path):
+        frames = []
+        with open(opus_path, 'rb') as f:
+            while True:
+                frame_len = int.from_bytes(f.read(4), byteorder='big')
+                if frame_len == 0:
+                    break
+                frame = f.read(frame_len)
+                frames.append(frame)
+        return  frames
+    def opus_to_audio(self, opus_data: list[ bytes]) -> str:
+
+        output_file = ('output2.wav')
+        #初始化解码器
+        decode = opuslib_next.Decoder(self.opus_sample_rate,self.opus_channel_count)
+        pcm_data = []
+        for frame in opus_data:
+            try:
+                #解码
+                pcm_frame = decode.decode(frame,self.opus_frame_size )
+                pcm_data.append(pcm_frame)
+            except opuslib_next.OpusError as e:
+                print(f'解码失败:{e}')
+
+        with wave.open(output_file, 'wb') as wf:
+            wf.setnchannels(self.opus_channel_count)
+            wf.setsampwidth(self.opus_sample_width)
+            wf.setframerate(self.opus_sample_rate)
+            wf.writeframes(b''.join(pcm_data))
+
+
 if __name__ == '__main__':
     opus = Opus_Encoder()
     data = opus.audio_to_opus('output.mp3')
     print(data)
+    opus.save_opus_raw(data[0], 'output.opus')
+    data = opus.load_opus_raw('output.opus')
+    print(data)
+    opus.opus_to_audio(data)
